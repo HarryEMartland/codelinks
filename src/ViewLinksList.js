e5e825a7bde9d3ade589b098da7aaa716cd7ff33
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-10-12 21:29:16 +0000

    Deploy HarryEMartland/codelinks to github.com/HarryEMartland/codelinks.git:gh-pages

--- src/ViewLinksList.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import Grid from "@material-ui/core/Grid/Grid";
-import Link from "./Link";
-
-export default (props) => <Grid container spacing={24}>
-    {props.links.map((link) => <Link key={link.name} {...link} toggleTagFilter={props.toggleTagFilter}/>)}
-</Grid>
