264f3dd4b860f425395a60596a768245e4e659b0
martlandh <HarryMartland@Rentalcars.com>
2018-10-19 11:09:59 +0100

    added pwa manifest

--- webpack.common.js
+++ webpack.common.js
@@ -2,6 +2,8 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const webpack = require('webpack');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const WorkboxPlugin = require('workbox-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
 
 const htmlPlugin = new HtmlWebPackPlugin({
     template: "./src/index.html",
@@ -36,6 +38,19 @@ module.exports = {
             // and not allow any straggling "old" SWs to hang around
             clientsClaim: true,
             skipWaiting: true
+        }),
+        new WebpackPwaManifest({
+            name: 'LearnTo.Codes',
+            short_name: 'LTC',
+            description: 'A collection of links to help you learn new languages and technologies including javascript, python, kubernetes and more.',
+            background_color: 'white',
+            crossorigin: 'use-credentials', //can be null, use-credentials or anonymous
+            icons: [
+                {
+                    src: path.resolve('./src/assets/icon.png'),
+                    sizes: [96, 128, 192, 256, 384, 512] // multiple sizes
+                }
+            ]
         })
     ]
 };
