258aa05299ca8dac8b15b354680c2b7107efbd18
martlandh <HarryMartland@Rentalcars.com>
2018-10-24 23:12:24 +0100

    added favicon

--- webpack.common.js
+++ webpack.common.js
@@ -4,19 +4,18 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const WorkboxPlugin = require('workbox-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
+
 
 const htmlPlugin = new HtmlWebPackPlugin({
     template: "./src/index.html",
-    filename: "./index.html"
+    title: '://LearnTo.Codes',
 });
 
 module.exports = {
     output: {},
     devServer: {
         hot: true,
-        proxy: {
-            '/drs': 'http://localhost:8080'
-        }
     },
     module: {
         rules: [
@@ -33,6 +32,7 @@ module.exports = {
         new CleanWebpackPlugin(['dist']),
         htmlPlugin,
         new webpack.HotModuleReplacementPlugin(),
+        new FaviconsWebpackPlugin('./src/assets/icon.png'),
         new WorkboxPlugin.GenerateSW({
             // these options encourage the ServiceWorkers to get in there fast
             // and not allow any straggling "old" SWs to hang around
