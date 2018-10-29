5bfd20667826292c6017dd0d5b3636552992512d
martlandh <HarryMartland@Rentalcars.com>
2018-10-29 20:28:23 +0000

    added git book

--- src/data/links.mjs
+++ src/data/links.mjs
@@ -285,8 +285,17 @@ export default [
     "name": "Learn Git Branching",
     "link": "learngitbranching.js.org",
     "tags": [
-      "git", "tutorial"
+      "git",
+      "tutorial"
     ],
     "description": "Interested in learning Git? Well you've come to the right place! \"Learn Git Branching\" is the most visual and interactive way to learn Git on the web; you'll be challenged with exciting levels, given step-by-step demonstrations of powerful features, and maybe even have a bit of fun along the way."
+  },
+  {
+    "name": "Git Book",
+    "link": "git-scm.com/book/en/v2",
+    "tags": [
+      "git"
+    ],
+    "description": ""
   }
 ]
